3bfde3c41e30ada7d3a4e32752e59aa5980cde61
JoeMachiato <TonyPlotTwister@gmail.com>
2026-03-02 09:37:37 +0100

    Refactor ANARC05B solution for clarity and efficiency
    Refactor logic to handle remaining elements in arrays and simplify code structure.

--- AP_SzwankowskiA_z1_helix.cpp
+++ AP_SzwankowskiA_z1_helix.cpp
@@ -1,7 +1,5 @@
 // ANARC05B //
 #include <cstdio>
-#include <climits>
-
 #define MAX(a,b) ((a)>(b)?(a):(b))
 
 int buffA[10001], buffB[10001];
@@ -10,29 +8,29 @@ int main() {
     int n1, n2;
     freopen("plik.in", "r", stdin);
     freopen("plik.out", "w", stdout);
-
+    
     while (scanf("%d", &n1) == 1 && n1 != 0) {
         for (int i = 0; i < n1; i++) scanf("%d", &buffA[i]);
-        buffA[n1] = INT_MAX;
-
         scanf("%d", &n2);
         for (int i = 0; i < n2; i++) scanf("%d", &buffB[i]);
-        buffB[n2] = INT_MAX;
-
         long long sumA = 0, sumB = 0, result = 0;
         int i = 0, j = 0;
-
         while (i < n1 && j < n2) {
-            if (buffA[i] != buffB[j]) {buffA[i] < buffB[j] ? sumA += buffA[i++] : sumB += buffB[j++];} 
-            else {
-                result += MAX(sumA, sumB) + (buffA[i] == INT_MAX ? 0 : buffA[i]);
-                if(buffA[i] == INT_MAX) break;
+            if (buffA[i] != buffB[j]) {
+                buffA[i] < buffB[j] ? sumA += buffA[i++] : sumB += buffB[j++];
+            } else {
+                result += MAX(sumA, sumB) + buffA[i];
                 sumA = sumB = 0;
                 i++; j++;
             }
         }
+        while (i < n1) sumA += buffA[i++];
+        while (j < n2) sumB += buffB[j++];
+        result += MAX(sumA, sumB);
+        
         printf("%lld\n", result);
     }
     return 0;
 }
+
 
